241b6309807393fb9835ad9c2274706f97d0cb9e
shaunmark <shaunantony1000@gmail.com>
2023-07-02 21:56:10 +0530

    - Added 'Plus Jakarta Sans' font and added it globally  - Added global styles file and imported to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+// import {} from './firebase/init';
 import AppRoutes from './routes';
+
+import './global.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
